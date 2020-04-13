012c40a7f84cc40af2926c039820593e521e7d3f
bogodars <bogodar@gmail.com>
2020-04-13 23:17:58 +0300

    first commit

---
+++ scripts.js
@@ -0,0 +1,12 @@
+//start
+
+const selectElement = (s) => document.querySelector(s);
+
+selectElement('.open').addEventListener('click', () => {
+    selectElement('.nav-list').classList.add('active')
+});
+
+
+selectElement('.close').addEventListener('click', () => {
+    selectElement('.nav-list').classList.remove('active');
+});
